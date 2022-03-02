26b79bfc51331f8dd028550576c4130fed3f82ae
somesay8507 <somesay8507@gmail.com>
2022-03-02 19:49:44 +0900

    페이지삭제기능End

--- Nodejs-master/main.js
+++ Nodejs-master/main.js
@@ -39,7 +39,7 @@ app.get('/page/:pageId', function(request, response) {
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
+          <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -121,11 +121,25 @@ app.post('/update_process', function(request,response){
       var title = post.title;
       var description = post.description;
       fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){;
+          response.redirect(`/?id=${title}`);
         })
       });
+  });
+});
+
+app.post('/delete_process', function(request,response){
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function(error){
+        response.redirect('/');
+      })
   });
 });
 
@@ -157,19 +171,7 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/update_process'){
      
     } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          })
-      });
+     
     } else {
       response.writeHead(404);
       response.end('Not found');
